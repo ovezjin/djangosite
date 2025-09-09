e1e81a33e5d317dbf53b6ca7e949a090f14ec10b
ovezjin <ovezjin@outlook.com>
2025-09-09 13:50:53 +0800

    add shortcut & remove hard in temp & namespace url

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
 
 # Create your views here.
 def index(request):
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    # output_text = ', '.join([q.question_text for q in latest_question_list])
+    # # return HttpResponse("Hello, world. You're at the polls index.")
+    # context = {"latest_question_list": latest_question_list}
+    # # return HttpResponse(output_text)
+    # return HttpResponse(template.render(context, request))
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    output_text = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse("Hello, world. You're at the polls index.")
     context = {"latest_question_list": latest_question_list}
-    # return HttpResponse(output_text)
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    '''
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question not exist")
+    '''
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+    # return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
